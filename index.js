1986c98c882100e9d8c6ca2a63bd92dba7ab5037
David Drummer <david.b.drummer@gmail.com>
2022-02-01 13:37:02 -0500

    commit tree 115bc985a9ef9c2672da68909a09a6dbd52ef09e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+// create function: saturdayFun
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// create function: mondayWork
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`;
+}
+
+// create function: wrapAdjective
+
+function wrapAdjective(flair = '*') {
+    return function(adj = "special") {
+        return `You are ${flair}${adj}${flair}!`;
+    };
+};
+
